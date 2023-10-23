eab73e0fb072e35f533818982b112bebcde7b9f8
Oshawk <oshawk@protonmail.com>
2023-10-23 20:10:02 +0100

    Make dropdown better

--- src/app.rs
+++ src/app.rs
@@ -1,6 +1,24 @@
+use crate::dropdown::Dropdown;
 use yew::prelude::*;
+
+#[derive(Clone, Copy, PartialEq)]
+pub enum Test {
+    A,
+    B,
+}
+
+impl ToString for Test {
+    fn to_string(&self) -> String {
+        match self {
+            Test::A => "A".to_string(),
+            Test::B => "B".to_string(),
+        }
+    }
+}
 
 #[function_component]
 pub fn App() -> Html {
-    html!(<h1></h1>)
+    html!(
+        <Dropdown<Test> items={ vec![Test::A, Test::B] } updated={ Callback::<Test>::from(|_| {}) }/>
+    )
 }
--- src/dropdown.rs
+++ src/dropdown.rs
@@ -1,12 +1,15 @@
 use yew::prelude::*;
 
 #[derive(Properties, PartialEq)]
-pub struct DropdownProperties {
-    pub items: Vec<String>,
+pub struct DropdownProperties<T: Copy + PartialEq + ToString + 'static> {
+    pub items: Vec<T>,
+    pub updated: Callback<T>,
+    #[prop_or("Select".to_string())]
+    pub prompt: String,
 }
 
 #[function_component]
-fn Dropdown(properties: &DropdownProperties) -> Html {
+pub fn Dropdown<T: Copy + PartialEq + ToString + 'static>(properties: &DropdownProperties<T>) -> Html {
     let active: UseStateHandle<bool> = use_state_eq(|| false);
     let active_true: Callback<FocusEvent> = {
         let active: UseStateHandle<bool> = active.clone();
@@ -17,7 +20,7 @@ fn Dropdown(properties: &DropdownProperties) -> Html {
         Callback::from(move |_| active.set(false))
     };
 
-    let selected: UseStateHandle<Option<String>> = use_state_eq(|| None);
+    let selected: UseStateHandle<Option<T>> = use_state_eq(|| None);
 
     html!(
         <div class="field">
@@ -25,9 +28,26 @@ fn Dropdown(properties: &DropdownProperties) -> Html {
                 <div class="control">
                     <div class="dropdown-trigger">
                         <button class="button is-justify-content-space-between" onfocus={ active_true } onblur={ active_false }>
-                            <span>{ match (*selected).clone() { Some(selected) => selected, None => "".to_string() } }</span>
+                            <span>{ match &*selected { Some(selected) => selected.to_string(), None => properties.prompt.clone() } }</span>
                             <span class="material-symbols-outlined">{ "expand_more" }</span>
                         </button>
+                    </div>
+                </div>
+                <div class="dropdown-menu" style="width:100%">
+                    <div class="dropdown-content">
+                        {
+                            properties.items.iter().map(|item| {
+                                let selected: UseStateHandle<Option<T>> = selected.clone();
+                                let updated: Callback<T> = properties.updated.clone();
+                                let item: T = *item;
+                                html!{
+                                    <a class="dropdown-item" onmousedown={ Callback::<MouseEvent>::from(move |_| {
+                                        selected.set(Some(item));
+                                        updated.emit(item);
+                                    }) }>{ item.to_string() }</a>
+                                }
+                            }).collect::<Html>()
+                        }
                     </div>
                 </div>
             </div>
